aa8ce7a8100c4bcb5bc75bae4c19f1982b2b2b1f
Tuhin35 <ariaan59@gmail.com>
2023-12-12 23:39:21 +0600

    update logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-scroll';
-
+import logo from '../assets/logo.png'
 const Header = () => {
   return <header className='py-2'>
     <div className='container mx-auto'>
@@ -8,7 +8,7 @@ const Header = () => {
         <h1
           className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
         >
-         RUT
+      <img className='cover h-24 rounded-3xl' src={logo} alt="" />
         </h1>
          <Link to='contact'> <button className='btn btn-sm'>Work with me</button></Link>
       </div>
